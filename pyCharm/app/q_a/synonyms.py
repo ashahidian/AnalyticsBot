6b388084fc19b53fd07a1f98a54e80b1906ebb32
ashahidian <anisa.shahidian@tecnico.ulisboa.pt>
2018-06-04 14:18:31 +0100

    file cleanup

---
+++ pyCharm/app/q_a/synonyms.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+
+def time_synonyms(word):
+
+    if word == 'yesterday':
+        yesterday = datetime.today() - timedelta(days=1)
+        return yesterday
+
+    if word == 'this year':
+        # calculating the current year
+        now = datetime.now()
+        return now.year
+
+    if word == 'last year':
+        now = datetime.now()
+        last_year = now - timedelta(days=365)
+        return last_year.year
+
+# there may be a better way to do this
+#     if word == 'first quarter':
+#         return ['january', 'february', 'march']
+#
+#     if word == 'second quarter':
+#         return ['april', 'may', 'june']
+#
+#     if word == 'third quarter':
+#         return ['july', 'august', 'september']
+#
+#     if word == 'fourth quarter':
+#         return ['october', 'november', 'december']
+#
+#     if word == 'last quarter':
+#         now = datetime.now()
+#         now_month = now.month
+#         last_quarter_month = now_month - 4
+#
+#         if 4 > last_quarter_month:
+#             last_quarter = 'first_quarter'
+#         elif 4 <= last_quarter_month < 6:
+#             last_quarter = 'second quarter'
+#         elif 6 <= last_quarter_month < 9:
+#             last_quarter = 'third quarter'
+#         else:
+#             last_quarter = 'fourth quarter'
+#
+#         return last_quarter
