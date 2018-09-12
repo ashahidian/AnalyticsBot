1d89ec7ea442fd82c5a7883c24321b355dc47809
ashahidian <anisa.shahidian@tecnico.ulisboa.pt>
2018-09-12 10:31:02 +0100

    time expressions

--- pyCharm/app/processing/time_expressions.py
+++ pyCharm/app/processing/time_expressions.py
@@ -1,12 +1,10 @@
 from datetime import datetime, timedelta
-#import parsedatetime as pdt #pip
+# import parsedatetime as pdt #pip
 import re
 
 
 def time_synonyms_simple(expression):
-
-    last_days = re.compile("last [0-9]+ days")
-    next_days = re.compile("next [0-9]+ days")
+    matched = re.search(r"(next|last)\s([0-9\s]*)(day|week|year|quarter)")
 
     if expression == 'yesterday':
         yesterday = "{:%Y-%m-%d}".format(datetime.today() - timedelta(days=1))
@@ -39,11 +37,99 @@ def time_synonyms_simple(expression):
         next_year = now + timedelta(days=365)
         return [str(next_year.year) + str("-01-01"), str(next_year.year) + str("-12-31")]
 
-    elif last_days.match(expression):
-        print "matched"
+    elif matched.group(1) == 'next':
+        if matched.group(3) == 'day':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(days=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(days=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+        elif matched.group(3) == 'week':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+        elif matched.group(3) == 'year':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number * 52))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number * 52))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+    elif matched.group(1) == 'last':
+        if matched.group(3) == 'day':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() - timedelta(days=number))
+                return [other, "{:%Y-%m-%d}".format(datetime.today())]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(days=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+        elif matched.group(3) == 'week':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() - timedelta(weeks=number))
+                return [other, "{:%Y-%m-%d}".format(datetime.today())]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
+
+        elif matched.group(3) == 'year':
+            if matched.group(2) != "":
+                number = matched.group(2)
+                other = "{:%Y-%m-%d}".format(datetime.today() - timedelta(weeks=number * 52))
+                return [other, "{:%Y-%m-%d}".format(datetime.today())]
+
+            else:
+                number = 1
+                other = "{:%Y-%m-%d}".format(datetime.today() + timedelta(weeks=number * 52))
+                return ["{:%Y-%m-%d}".format(datetime.today()), other]
 
 
-#def time_synonyms(expression):
+def calculate_last_quarter():
+    now = datetime(2018, 7, 28)
+    quarter = (now.month - 1) / 3 + 1
+    last_day_last_quarter = datetime(now.year, 3 * quarter - 2, 1)
+    first_day_last_quarter = datetime(last_day_last_quarter.year if last_day_last_quarter.month - 3 >= 0
+                                      else last_day_last_quarter.year - 1, (last_day_last_quarter.month - 3) % 12, 1)
 
-   # date = datetime.datetime(*time_struct[:3])
-   # return "{:%Y-%m-%d}".format(date)
+    return [first_day_last_quarter, last_day_last_quarter]
+
+
+def calculate_next_quarter():
+    now = datetime.now()
+    current_quarter = (now.month - 1) / 3 + 1
+    future_quarter = (current_quarter + 4) % 4 + 1
+    future_quarter_first = datetime(now.year if future_quarter != 1
+                                    else now.year + 1, (current_quarter * 3 + 1) % 12, 1)
+    future_quarter_last = datetime(now.year + 1 if (future_quarter == 1) or (future_quarter == 4)
+                                   else now.year, (future_quarter * 3 + 1) % 12, 1)
+
+    return [future_quarter_first, future_quarter_last]
+
+
+#if __name__ == '__main__':
+#    last = calculate_last_quarter()
+#    next = calculate_next_quarter()
+
+#    print(last, next)
--- pyCharm/app/rules/parsetab.py
+++ pyCharm/app/rules/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'DIMENSION EXCLUDE MEASURE METRIC TOKEN statement : METRIC TOKEN\n                | DIMENSION TOKEN  statement : TOKEN METRIC\n                | TOKEN DIMENSION  statement : METRIC TOKEN EXCLUDE\n                | DIMENSION TOKEN EXCLUDE  statement : EXCLUDE METRIC TOKEN\n                | EXCLUDE DIMENSION TOKEN  statement : DIMENSION TOKEN METRIC\n                | METRIC TOKEN DIMENSION\n                | DIMENSION TOKEN DIMENSION  statement : DIMENSION METRIC TOKEN\n                | METRIC DIMENSION TOKEN\n                | DIMENSION DIMENSION TOKEN  statement : DIMENSION MEASURE METRIC\n                | METRIC MEASURE DIMENSION '
+_lr_signature = 'DIMENSION EXCLUDE MEASURE METRIC TOKEN statement : METRIC TOKEN\n                | DIMENSION TOKEN  statement : TOKEN METRIC\n                | TOKEN DIMENSION  statement : METRIC TOKEN EXCLUDE\n                | DIMENSION TOKEN EXCLUDE  statement : EXCLUDE METRIC TOKEN\n                | EXCLUDE DIMENSION TOKEN  statement : DIMENSION TOKEN METRIC\n                | METRIC TOKEN DIMENSION\n                | DIMENSION TOKEN DIMENSION  statement : DIMENSION METRIC TOKEN\n                | METRIC DIMENSION TOKEN\n                | DIMENSION DIMENSION TOKEN  statement : DIMENSION TOKEN METRIC TOKEN\n                | METRIC TOKEN DIMENSION TOKEN\n                | DIMENSION TOKEN DIMENSION TOKEN statement : DIMENSION MEASURE METRIC\n                | METRIC MEASURE DIMENSION\n                | DIMENSION MEASURE DIMENSION '
     
-_lr_action_items = {'METRIC':([0,2,4,5,14,16,],[1,9,11,13,24,28,]),'TOKEN':([0,1,5,8,11,12,13,15,],[2,7,14,20,21,22,23,27,]),'MEASURE':([1,5,],[6,16,]),'EXCLUDE':([0,7,14,],[4,18,25,]),'DIMENSION':([0,1,2,4,5,6,7,14,],[5,8,10,12,15,17,19,26,]),'$end':([3,7,9,10,14,17,18,19,20,21,22,23,24,25,26,27,28,],[0,-1,-3,-4,-2,-16,-5,-10,-13,-7,-8,-12,-9,-6,-11,-14,-15,]),}
+_lr_action_items = {'METRIC':([0,2,4,5,14,16,],[1,9,11,13,24,28,]),'TOKEN':([0,1,5,8,11,12,13,15,19,24,26,],[2,7,14,20,21,22,23,27,30,31,32,]),'MEASURE':([1,5,],[6,16,]),'EXCLUDE':([0,7,14,],[4,18,25,]),'DIMENSION':([0,1,2,4,5,6,7,14,16,],[5,8,10,12,15,17,19,26,29,]),'$end':([3,7,9,10,14,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,],[0,-1,-3,-4,-2,-19,-5,-10,-13,-7,-8,-12,-9,-6,-11,-14,-18,-20,-16,-15,-17,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -27,20 +27,24 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statement","S'",1,None,None,None),
-  ('statement -> METRIC TOKEN','statement',2,'p_statement_single','grammar.py',45),
-  ('statement -> DIMENSION TOKEN','statement',2,'p_statement_single','grammar.py',46),
-  ('statement -> TOKEN METRIC','statement',2,'p_statement_single_second','grammar.py',52),
-  ('statement -> TOKEN DIMENSION','statement',2,'p_statement_single_second','grammar.py',53),
+  ('statement -> METRIC TOKEN','statement',2,'p_statement_single','grammar.py',46),
+  ('statement -> DIMENSION TOKEN','statement',2,'p_statement_single','grammar.py',47),
+  ('statement -> TOKEN METRIC','statement',2,'p_statement_single_second','grammar.py',53),
+  ('statement -> TOKEN DIMENSION','statement',2,'p_statement_single_second','grammar.py',54),
   ('statement -> METRIC TOKEN EXCLUDE','statement',3,'p_statement_exclude','grammar.py',59),
   ('statement -> DIMENSION TOKEN EXCLUDE','statement',3,'p_statement_exclude','grammar.py',60),
-  ('statement -> EXCLUDE METRIC TOKEN','statement',3,'p_statement_exclude_first','grammar.py',66),
-  ('statement -> EXCLUDE DIMENSION TOKEN','statement',3,'p_statement_exclude_first','grammar.py',67),
-  ('statement -> DIMENSION TOKEN METRIC','statement',3,'p_statement_multiple_token_second','grammar.py',73),
-  ('statement -> METRIC TOKEN DIMENSION','statement',3,'p_statement_multiple_token_second','grammar.py',74),
-  ('statement -> DIMENSION TOKEN DIMENSION','statement',3,'p_statement_multiple_token_second','grammar.py',75),
-  ('statement -> DIMENSION METRIC TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',81),
-  ('statement -> METRIC DIMENSION TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',82),
-  ('statement -> DIMENSION DIMENSION TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',83),
-  ('statement -> DIMENSION MEASURE METRIC','statement',3,'p_statement_measure','grammar.py',89),
-  ('statement -> METRIC MEASURE DIMENSION','statement',3,'p_statement_measure','grammar.py',90),
+  ('statement -> EXCLUDE METRIC TOKEN','statement',3,'p_statement_exclude_first','grammar.py',65),
+  ('statement -> EXCLUDE DIMENSION TOKEN','statement',3,'p_statement_exclude_first','grammar.py',66),
+  ('statement -> DIMENSION TOKEN METRIC','statement',3,'p_statement_multiple_token_second','grammar.py',71),
+  ('statement -> METRIC TOKEN DIMENSION','statement',3,'p_statement_multiple_token_second','grammar.py',72),
+  ('statement -> DIMENSION TOKEN DIMENSION','statement',3,'p_statement_multiple_token_second','grammar.py',73),
+  ('statement -> DIMENSION METRIC TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',78),
+  ('statement -> METRIC DIMENSION TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',79),
+  ('statement -> DIMENSION DIMENSION TOKEN','statement',3,'p_statement_multiple_token_third','grammar.py',80),
+  ('statement -> DIMENSION TOKEN METRIC TOKEN','statement',4,'p_statement_multiple_token_four','grammar.py',85),
+  ('statement -> METRIC TOKEN DIMENSION TOKEN','statement',4,'p_statement_multiple_token_four','grammar.py',86),
+  ('statement -> DIMENSION TOKEN DIMENSION TOKEN','statement',4,'p_statement_multiple_token_four','grammar.py',87),
+  ('statement -> DIMENSION MEASURE METRIC','statement',3,'p_statement_measure','grammar.py',93),
+  ('statement -> METRIC MEASURE DIMENSION','statement',3,'p_statement_measure','grammar.py',94),
+  ('statement -> DIMENSION MEASURE DIMENSION','statement',3,'p_statement_measure','grammar.py',95),
 ]
